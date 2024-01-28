58577b532e1ddc331f03cd0ea4d0cb1c74d25951
Bevis <abc123123437@gmail.com>
2024-01-28 13:52:51 +0000

    feat: add RBAC permissions for jobs

--- internal/controller/cronjob_controller.go
+++ internal/controller/cronjob_controller.go
@@ -48,6 +48,8 @@ type Clock interface {
 //+kubebuilder:rbac:groups=batch.tutorial.kubebuilder.io,resources=cronjobs,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=batch.tutorial.kubebuilder.io,resources=cronjobs/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=batch.tutorial.kubebuilder.io,resources=cronjobs/finalizers,verbs=update
+//+kubebuilder:rbac:groups=batch,resources=jobs,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=batch,resources=jobs/status,verbs=get
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
