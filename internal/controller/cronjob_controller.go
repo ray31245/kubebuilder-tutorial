75a6d94803999c082c0f9fab8d671da11fa1eaa4
Bevis <abc123123437@gmail.com>
2024-01-28 13:55:46 +0000

    feat: declare a const for title of annotation

--- internal/controller/cronjob_controller.go
+++ internal/controller/cronjob_controller.go
@@ -51,6 +51,10 @@ type Clock interface {
 //+kubebuilder:rbac:groups=batch,resources=jobs,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=batch,resources=jobs/status,verbs=get
 
+var (
+	scheduledTimeAnnotation = "batch.tutorial.kubebuilder.io/scheduled-at"
+)
+
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 // TODO(user): Modify the Reconcile function to compare the state specified by
